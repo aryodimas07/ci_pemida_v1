e65d50729448cbc8fe75abe16b59c1d712007b96
unknown <aryodimas07@gmail.com>
2018-07-19 09:40:10 +0700

    Make User_Model

---
+++ application/models/User_Model.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User_Model extends CI_Model
+{
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    /**
+     * Method untuk mengambil data user berupa username/email, password dan role
+     * @param  string $email email dari user
+     * @return array     data user berupa email, password dan role
+     */
+    public function get_user_info($email)
+    {
+        $this->db->select('email', 'password', 'isAdmin');
+        $this->db->from('user');
+        $this->db->where('email', $email);
+        $this->db->limit(1);
+        return $query->row_array();
+    }
+}
