84edda011e19c865f6bf5d50ee1056b1c95cb18f
unknown <aryodimas07@gmail.com>
2018-07-19 09:39:47 +0700

    Auth Controller change on __construct() and index()

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,22 +1,30 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
 
-  public function __construct()
-        {
-                parent::__construct();
-                $this->load->model('news_model');
-                $this->load->helper('url_helper');
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('user_model');
+        $this->load->library('session');
+        $this->load->helper('url_helper');
+    }
+
+    public function index()
+    {
+        if (isset($this->session->userdata()['logged_in'])) {
+            $this->load->view('app/index');
+        } else {
+            //$this->load->view('login/index');
+            $this->user_model->get_user_info('dimas');
         }
+    }
 
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function login()
+    {
 
-  public function login()
-  {
-    //
-  }
+    }
 }
