ed3d72f65ebab420b8c6cfcf840377d1c7619017
unknown <aryodimas07@gmail.com>
2018-07-19 10:21:51 +0700

    Add login page

--- application/views/auth/loginPage.php
+++ application/views/auth/loginPage.php
@@ -1,267 +1,79 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 <head>
-  <title>HOME</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>CAPEXRT</title>
+  <!--bootstrap.css-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-  <script src="https://chartjs-plugin-datalabels.netlify.com/chartjs-plugin-datalabels.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
   <style media="screen">
-  #table {
-    font-size: small;
-  }
+    html, body{
+      height: 100%;
+    }
 
-  .section-header {
-    border-style: solid;
-    border-width: 0px 0px 1px 0px;
-    border-color: lightgrey;
-    padding-bottom: 10px;
-    margin-bottom: 20px;
-  }
-  #table td {
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-  }
-  #table #new td:hover, #table #co td:hover {
-    background-color: lightgrey;
-    border-style: solid;
-    border-width: 2px;
-    border-color: blue;
-    box-sizing: border-box;
-  }
+    body {
+      display: -ms-flexbox;
+      display: flex;
+      -ms-flex-align: center;
+      align-items: center;
+      padding-top: 40px;
+      padding-bottom: 40px;
+      background-color: #f0e5dd;
+    }
 
-  #table input {
-    width: 50px;
-    border: none;
-    background-color:rgba(0, 0, 0, 0);
-  }
+    .form-signin {
+      width: 100%;
+      max-width: 330px;
+      padding: 15px;
+      margin: auto;
+    }
 
-  #table input:focus {
-    background-color: lightgrey;
-    font-weight: bold;
-  }
+    .form-signin .form-control {
+      position: relative;
+      box-sizing: border-box;
+      height: auto;
+      padding: 10px;
+      font-size: 16px;
+    }
 
-  #table {
-    webkit-box-sizing: border-box;
-    moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
+    .form-signin .form-control:focus {
+      z-index: 2;
+    }
 
-  #formTitle {
-    width: 500px;
-  }
+    .form-signin input[type="email"] {
+      margin-bottom: -1px;
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+    }
 
-  .chartContainer {
-    width: 800px;
-    height: 400px;
-  }
+    .form-signin input[type="password"] {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
 
-  #resizable {
-    padding-bottom: 35px;
-  }
-
-  canvas {
-    margin: auto;
-  }
-
+    .title {
+      color: red;
+    }
   </style>
 </head>
 <body>
-  <div class="main-wrapper">
-    <main role="main" class="container-fluid">
-      <h2 class="section-header">CAPEX DIT</h2>
-      <!--DATA TABLE-->
-      <table id="table" class="table table-bordered table-striped">
-        <tr class="table-primary">
-          <td></td>
-          <th scope="col">COMM RKAP</th>
-          <th scope="col">COMM REL (n-1)</th>
-          <th scope="col">COMM REL</th>
-          <th scope="col">PR (n-1)</th>
-          <th scope="col">PR</th>
-          <th scope="col">PO (n-1)</th>
-          <th scope="col">PO</th>
-          <th scope="col">PAY RKAP</th>
-          <th scope="col">PAY REL</th>
-          <th scope="col">ACTUAL (n-1)</th>
-          <th scope="col">ACTUAL</th>
-        </tr>
-        <tr id="new">
-          <th scope="row">NEW</th>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-        </tr>
-        <tr id="co">
-          <th scope="row">CO</th>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-          <td><input type="text" value="150"></td>
-        </tr>
-        <tr>
-          <th scope="row">Total</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </table>
-
-      <!--UPDATE CHART BUTTON-->
-      <button id="updateData" class="btn btn-primary">Update Data</button>
-
-      <!--Aspect Ratio-->
-      <select id="aspectRatioDD">
-        <option value="4 / 3">4 : 3</option>
-        <option value="16 / 9">16 : 9</option>
-        <option value="1 / 1">1 : 1</option>
-      </select>
-      <input type="text" name="" value="">
-
-      <!--CHART-->
-      <h2 class="section-header">Chart</h2>
-      <div id="resizable" class=" chartContainer ui-widget-content">
-        <canvas id="myChart" height="200" width="400"></canvas>
-      </div>
-
-    </main>
-
+  <div class="container-fluid text-center">
+    <h1 class="title h1">CAPEXRT</h1>
+    <form class="form-signin" action=<?php echo site_url('login'); ?> method="post" autocomplete="off">
+      <h1 class="h3 mb-3 font-weight-normal">Mohon login terlebih dahulu</h1>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Alamat email" required aut
+      >
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" name="password" id="inputPassword" class="form-control mb-3" placeholder="Password" required aut
+      >
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
   </div>
-  <!--TITLE-->
-  <!--<script src="https://chartjs-plugin-datalabels.netlify.com/samples/utils.js"></script>-->
-  <script type="text/javascript">
-  $(function() {
-    $("#resizable").resizable({
-      minWidth: 730
-    });
-  });
 
-  $('#table, #titleText').bind('keydown', function(event) {
-    if (event.keyCode == 13) {
-      refreshData();
-    }
-  });
-
-  var ctx = document.getElementById("myChart");
-  var table = document.getElementById("table");
-
-  //var data NEW & CO
-  var dataNEW = [];
-  var dataCO = [];
-
-  //value table -> var data
-  for (var i = 1; i <= table.rows[0].cells.length - 1; i = i + 1) {
-    dataNEW.push(table.rows[1].cells[i].childNodes[0].value);
-    dataCO.push(table.rows[2].cells[i].childNodes[0].value);
-  }
-
-
-  var NEW = {
-    label: 'NEW',
-    data: dataNEW,
-    backgroundColor: 'orange',
-    borderWidth: 0
-  };
-  var CO = {
-    label: 'CO',
-    data: dataCO,
-    backgroundColor: 'blue',
-    borderWidth: 0
-  };
-  var Total = {
-    label: 'TOTAL',
-    data: [200, 150, 150, 150, 150, 10, 20, 130, 0, 0, 0],
-    backgroundColor: 'grey',
-    borderWidth: 0
-  };
-  var DATA = {
-    labels: ["COMM RKAP", "COMM REL (n-1)", "COMM REL", "PR (n-1)", "PR", "PO (n-1)", "PO", "PAY RKAP", "PAY REL", "ACTUAL (n-1)", "ACTUAL"],
-    datasets: [CO, NEW]
-  };
-
-  var inputTitle = document.getElementById("titleText");
-
-  var OPTION = {
-    title: {
-      display: true,
-      text: "Custom Title",
-      fontSize: 20
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      xAxes: [{
-        stacked: true,
-        ticks: {
-          fontSize: 11
-        }
-      }],
-      yAxes: [{
-        stacked: true,
-      }]
-    },
-    plugins: {
-      datalabels: {
-        color: 'white',
-        display: function(context) {
-          return context.dataset.data[context.dataIndex] > 15;
-        },
-        font: {
-          weight: 'bold'
-        },
-        formatter: Math.round
-      }
-    }
-  };
-
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: DATA,
-    options: OPTION
-  });
-
-  document.getElementById('updateData').addEventListener('click', function() {
-    refreshData();
-  });
-
-  function refreshData() {
-    dataNEW.splice(0, dataNEW.length);
-    dataCO.splice(0, dataCO.length);
-    for (var i = 1; i <= table.rows[0].cells.length - 1; i = i + 1) {
-      dataNEW.push(table.rows[1].cells[i].childNodes[0].value);
-      dataCO.push(table.rows[2].cells[i].childNodes[0].value);
-    }
-    //Chart.options.title.text = "New Title"
-    window.myChart.update();
-  }
-
-  </script>
+  <!--jQuery, Popper.js, bootstrap.js-->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
