e79c591725ddbb6f967b0a82872649bf64886d83
unknown <aryodimas07@gmail.com>
2018-07-25 09:49:48 +0700

    change on function's name and make some function to get program info and pic

--- application/models/Program_Model.php
+++ application/models/Program_Model.php
@@ -8,42 +8,54 @@ class Program_Model extends CI_Model
         $this->load->database();
     }
 
-    public function get_program_info($email)
+    public function get_program_relate($email)
     {
-        if ($this->is_admin($email)) {
+        if (is_admin()) {
           //ambil data nama dan tanggal pembuatan dari semua program
-          $this->db->select('nama, dateCreated');
+          $this->db->select('nama, dateCreated, deskripsi, slug');
           $this->db->from('program');
           $query = $this->db->get();
           return $query->result_array();
         } else {
           //ambil data nama dan tanggal pembuatan dari program bersangkutan
-          $this->db->select('program.nama, program.dateCreated');
+          $this->db->select('program.nama, program.dateCreated, program.deskripsi, program.slug');
           $this->db->from('user');
           $this->db->join('program_pic', 'user.id = program_pic.id_pic');
           $this->db->join('program', 'program_pic.id_program = program.id');
+          $this->db->where('email', $email);
           $query = $this->db->get();
           return $query->result_array();
         }
     }
 
-    public function is_admin($email)
+    public function get_program_info($slug)
     {
-        $this->db->select('*');
-        $this->db->from('user');
-        $this->db->where('email', $email);
-        $this->db->where('isAdmin', 1);
+        $this->db->select('nama, dateCreated, deskripsi, slug');
+        $this->db->from('program');
+        $this->db->where('slug', $slug);
         $query = $this->db->get();
-        if ($query->num_rows() == 1) {
-            return true;
-        } else {
-            return false;
-        }
+        return $query->row_array();
     }
 
-    public function get_login_info()
+    public function get_program_pic($slug)
     {
-        $result = $this->session->userdata()['logged_in']['email'];
-        return $result;
+        $this->db->select('user.id, user.nama, user.email, user.notelp');
+        $this->db->from('program');
+        $this->db->join('program_pic', 'program.id = program_pic.id_program');
+        $this->db->join('user', 'program_pic.id_pic = user.id');
+        $this->db->where('program.slug', $slug);
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function is_program_exists($slug)
+    {
+      $this->db->select('program.slug');
+      $this->db->from('user');
+      $this->db->join('program_pic', 'user.id = program_pic.id_pic');
+      $this->db->join('program', 'program_pic.id_program = program.id');
+      $this->db->where('slug', $slug);
+      $query = $this->db->get();
+      return $query->result_array();
     }
 }
