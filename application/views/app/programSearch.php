1a6ff721c7cc892146aa61f4b63bf4c20547d29f
unknown <aryodimas07@gmail.com>
2018-07-30 13:11:05 +0700

    optimize the navbar

--- application/views/app/programSearch.php
+++ application/views/app/programSearch.php
@@ -3,20 +3,44 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#343a40">
   <title>PEMIDA : Pemantauan Permintaan Pengadaan</title>
   <!--bootstrap.css-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
   <title>PEMIDA</title>
+  <style media="screen">
+  .nav-sm {
+    display: none;
+  }
+
+  @media (max-width: 575.98px) {
+    .nav-sm {
+      display: block;
+    }
+    #nav-lg {
+      display: none;
+    }
+  }
+
+  #nav-search {
+    background: linear-gradient(to top, rgba(255,0,0,0), white);
+  }
+
+  #inputSearchFilter {
+
+  }
+
+  </style>
 </head>
 <body>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+  <nav id="nav-lg" class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         <img src=<?php echo site_url('assets/img/Asset1ldpi.png') ?> width="30" height="30" class="d-inline-block align-top" alt="">
         PEMIDA
       </a>
       <div class="input-group">
-        <input id="inputSearchFilter" onkeyup="searchFilter()" type="text" class="form-control" placeholder="Cari nama program...">
+        <input id="inputSearchFilter1" onkeyup="searchFilter1()" type="text" class="form-control" placeholder="Cari nama program...">
       </div>
       <ul class="nav nav-pills ml-3">
         <li class="nav-item">
@@ -25,8 +49,42 @@
       </ul>
     </div>
   </nav>
+  <nav class="nav-sm navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src=<?php echo site_url('assets/img/Asset1ldpi.png') ?> width="30" height="30" class="d-inline-block align-top" alt="">
+        PEMIDA
+      </a>
+      <ul class="nav nav-pills ml-3">
+        <li class="nav-item">
+          <a class="nav-link btn-danger" href=<?php echo site_url('logout'); ?>>Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <nav id="nav-search" class="nav-sm navbar navbar-expand-md sticky-top">
+    <div class="container-fluid">
+      <div class="input-group">
+        <input id="inputSearchFilter" onkeyup="searchFilter()" type="text" class=" shadow form-control form-control-lg" placeholder="Cari nama program...">
+      </div>
+    </div>
+  </nav>
   <script type="text/javascript">
   // Search Filter
+  function searchFilter1() {
+    var input, filter, div, list, a, i;
+    input = document.getElementById('inputSearchFilter1');
+    filter = input.value.toUpperCase();
+    div = document.getElementById('programList');
+    list = div.getElementsByTagName('a');
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].text.toUpperCase().indexOf(filter) > -1) {
+        list[i].style.display = "";
+      } else {
+        list[i].style.display = "none"
+      }
+    }
+  }
   function searchFilter() {
     var input, filter, div, list, a, i;
     input = document.getElementById('inputSearchFilter');
