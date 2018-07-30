b14422ea937ae15fa1edfc0375e006e6fa1c2678
unknown <aryodimas07@gmail.com>
2018-07-30 13:10:47 +0700

    add color on mobile, and fix the title

--- application/views/auth/loginPage.php
+++ application/views/auth/loginPage.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>CAPEXRT</title>
+  <meta name="theme-color" content="#343a40">
+  <title>PEMIDA</title>
   <!--bootstrap.css-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
   <style media="screen">
