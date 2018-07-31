367bd38cd6c6b8da71845498e4edfd3d8b6b01be
unknown <aryodimas07@gmail.com>
2018-07-31 09:18:50 +0700

    add query for get program info by status and email

--- application/models/Program_Model.php
+++ application/models/Program_Model.php
@@ -28,6 +28,28 @@ class Program_Model extends CI_Model
         }
     }
 
+    public function get_program_info_by_status_and_email($status, $email)
+    {
+      if (is_admin()) {
+        //ambil data nama dan tanggal pembuatan dari semua program
+        $this->db->select('nama, dateCreated, deskripsi, slug');
+        $this->db->from('program');
+        $this->db->where('status', $status);
+        $query = $this->db->get();
+        return $query->result_array();
+      } else {
+        //ambil data nama dan tanggal pembuatan dari program bersangkutan
+        $this->db->select('program.nama, program.dateCreated, program.deskripsi, program.slug');
+        $this->db->from('user');
+        $this->db->join('program_pic', 'user.id = program_pic.id_pic');
+        $this->db->join('program', 'program_pic.id_program = program.id');
+        $this->db->where('email', $email);
+        $this->db->where('status', $status);
+        $query = $this->db->get();
+        return $query->result_array();
+      }
+    }
+
     public function get_program_info($slug)
     {
         $this->db->select('*');
@@ -35,6 +57,15 @@ class Program_Model extends CI_Model
         $this->db->where('slug', $slug);
         $query = $this->db->get();
         return $query->row_array();
+    }
+
+    public function get_id_program_by_slug($slug)
+    {
+      $this->db->select('id');
+      $this->db->from('program');
+      $this->db->where('slug', $slug);
+      $query = $this->db->get();
+      return $query->row_array()['id'];
     }
 
     public function get_program_pic($slug)
@@ -70,7 +101,7 @@ class Program_Model extends CI_Model
       return $query->result_array();
     }
 
-    public function get_procurement($slug)
+    public function get_procurement()
     {
       $this->db->select('*');
       $this->db->from('procurement');
@@ -81,8 +112,8 @@ class Program_Model extends CI_Model
     public function set_status($slug, $status)
     {
       $this->db->set('status', $status);
-      $this->db->insert('program');
       $this->db->where('slug', $slug);
+      $this->db->update('program');
     }
 
     public function get_pic_role($user, $slug)
@@ -104,5 +135,24 @@ class Program_Model extends CI_Model
         );
         $this->db->where('id', $index);
         $this->db->update('program', $data);
+    }
+
+    public function create_information($proc, $slug)
+    {
+      $id_program = $this->get_id_program_by_slug($slug);
+      $data = array(
+        'id_program' => $id_program,
+        'id_procurement' => $proc,
+        'submitDate' => date('y-m-d')
+      );
+      $this->db->insert('informasi', $data);
+
+      //jika tahap proc sudah sampai "kontrak"
+      if ($proc == 9) {
+        $this->db->where('slug', $slug);
+        $this->db->update('program', array('status' => 1));
+      }
+
+
     }
 }
